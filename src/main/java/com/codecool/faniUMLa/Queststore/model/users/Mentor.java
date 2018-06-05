d07e048f62ae06005d18519f108744091ba08437
szymslow12 <szym.slow12@gmail.com>
2018-06-05 11:36:19 +0200

    add abstract User, change in Mentor -> extends User

--- src/main/java/com/codecool/faniUMLa/Queststore/model/users/Mentor.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/model/users/Mentor.java
@@ -1,4 +1,8 @@
 package com.codecool.faniUMLa.Queststore.model.users;
 
-public class Mentor {
+public class Mentor extends User{
+
+    public Mentor(int accountID, String firstName, String lastName, String email, String phoneNumber) {
+        super(accountID, firstName, lastName, email, phoneNumber);
+    }
 }
---
+++ src/main/java/com/codecool/faniUMLa/Queststore/model/users/User.java
@@ -0,0 +1,20 @@
+package com.codecool.faniUMLa.Queststore.model.users;
+
+public abstract class User {
+
+    private int accountID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String phoneNumber;
+    private String login;
+    private String password;
+
+    public User(int accountID, String firstName, String lastName, String email, String phoneNumber) {
+        this.accountID = accountID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+    }
+}
