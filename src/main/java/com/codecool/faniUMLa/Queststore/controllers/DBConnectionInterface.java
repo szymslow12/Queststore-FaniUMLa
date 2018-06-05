69ba07d70d66bb4df836ac403a7a93b671be0616
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-06-05 14:20:33 +0200

    connect to database

--- src/main/java/com/codecool/faniUMLa/Queststore/controllers/DBConnectionInterface.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/controllers/DBConnectionInterface.java
@@ -1,4 +1,22 @@
 package com.codecool.faniUMLa.Queststore.controllers;
 
-public class DBConnectionInterface {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public interface DBConnectionInterface {
+
+    default Connection connect() {
+        Connection c = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager
+                    .getConnection("jdbc:postgresql://localhost:5432/codecool_quest",
+                            "elzbietakrzych", "");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println(e.getClass().getName()+": "+e.getMessage());
+            System.exit(0);
+        }
+        return c;
+    }
 }
