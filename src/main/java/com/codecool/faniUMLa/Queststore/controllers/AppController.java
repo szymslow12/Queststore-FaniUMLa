7ed0d7ee2dd504ad0ffc26a6e03cbed1deaa7af4
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-06-06 09:48:23 +0200

    Implement AppController, handleMenu, run

--- src/main/java/com/codecool/faniUMLa/Queststore/controllers/AppController.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/controllers/AppController.java
@@ -1,4 +1,40 @@
 package com.codecool.faniUMLa.Queststore.controllers;
 
+import com.codecool.faniUMLa.Queststore.model.users.UserPriviledge;
+
 public class AppController {
+    ControllerCodecooler controllerCodecooler;
+    ControllerMentor controllerMentor;
+    ControllerAdmin controllerAdmin;
+
+    public AppController() {
+        controllerCodecooler = new ControllerCodecooler();
+        controllerMentor = new ControllerMentor();
+        controllerAdmin = new Controller (csvDAOEmployee, csvDAOStudent);
+        this.setCsvDAOEmployee(csvDAOEmployee);
+        this.setCsvDAOStudent(csvDAOStudent);
+        signIn();
+    }
+
+    public void handleMenu(UserPriviledge privilege) {
+        switch (privilege) {
+            case ADD_MENTOR:
+                employeeController.addMentor();
+                break;
+            case REMOVE_MENTOR:
+                employeeController.removeMentor();
+                break;
+            default:
+                super.errorMessage();
+        }
+    }
+
+    public void run() {
+        UserPriviledge privilege;
+        do {
+            displayMenu();
+            privilege = choosePrivilege();
+            handleMenu(privilege);
+        } while (isRun(privilege));
+    }
 }
