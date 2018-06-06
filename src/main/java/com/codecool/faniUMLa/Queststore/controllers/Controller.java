01369200c8de9ddd0bcbb3d75b9239751f1499b9
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-06-06 09:49:54 +0200

    create connection, implement choosePriviledge

--- src/main/java/com/codecool/faniUMLa/Queststore/controllers/Controller.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/controllers/Controller.java
@@ -4,12 +4,14 @@ import com.codecool.faniUMLa.Queststore.DAO.DAOUser;
 import com.codecool.faniUMLa.Queststore.DAO.DAOUserInterface;
 import com.codecool.faniUMLa.Queststore.View;
 import com.codecool.faniUMLa.Queststore.model.users.User;
+import com.codecool.faniUMLa.Queststore.model.users.UserPriviledge;
 
 import java.sql.Connection;
+import java.util.List;
 import java.util.Scanner;
 
 public class Controller extends DBConnectionController {
-    Connection connection = connect();
+    public Connection connection = connect();
     View view = new View();
     Scanner scanner = new Scanner(System.in);
     private static User user;
@@ -47,6 +49,18 @@ public class Controller extends DBConnectionController {
 
     private User getUser() {
         return user;
+    }
+
+    public UserPriviledge choosePrivilege() {
+        List<UserPriviledge> privileges = user.getAccess().getPrivileges();
+        Integer answer = Integer.valueOf(askUser("Which option would you like to choose(number)"));
+        for(int i = 0; i < privileges.size(); i++) {
+            if(answer.equals(i)) {
+                return privileges.get(i);
+            }
+        }
+        view.printLine("There's no such option!");
+        return choosePrivilege();
     }
 
 }
