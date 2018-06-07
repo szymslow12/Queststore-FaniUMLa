5473a43c848dc34b1983bcafd0e3f1fcbea1c84f
szymslow12 <szym.slow12@gmail.com>
2018-06-07 11:43:28 +0200

    add getCodecoolersWalletsQuery/ markQuestDoneQuery in progress

--- src/main/java/com/codecool/faniUMLa/Queststore/DAO/DAOMentorHelper.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/DAO/DAOMentorHelper.java
@@ -15,7 +15,7 @@ public class DAOMentorHelper {
                 "Enter codecooler email: ", "Enter codecooler phone number: ", "Enter ID of class: "};
         String query = String.format("%s%s%s", "INSERT INTO codecoolers (first_name, last_name, email, ",
                 "phone_number, coolcoins, level_of_exp, id_class)\n", "VALUES ('%s', '%s', '%s', '%s', 0, 0, %s)");
-        String[] queryValues = getQueryValues(messages.length, messages);
+        String[] queryValues = getQueryValues(messages);
         return String.format(query, queryValues[0], queryValues[1], queryValues[2], queryValues[3], queryValues[4]);
     }
 
@@ -25,7 +25,7 @@ public class DAOMentorHelper {
                 "Enter amount of coolcoins for quest: ", "Enter description: "};
         String query = String.format("%s%s", "INSERT INTO quests (id_category, quest_name, award, description)\n",
                 "VALUES (%s, '%s', %s, '%s')");
-        String[] queryValues = getQueryValues(messages.length, messages);
+        String[] queryValues = getQueryValues(messages);
         return String.format(query, queryValues[0], queryValues[1], queryValues[2], queryValues[3]);
     }
 
@@ -33,17 +33,38 @@ public class DAOMentorHelper {
     public String getAddNewArtifactQuery() {
         String[] messages = {"Enter artifact name: ", "Enter artifact category: ",
                 "Enter price: ", "Enter description: "};
-        String query = String.format("%s%s", "INSERT INTO store (artifact_name, category, price, description)\n",
+        String query = String.format("%s%s", "INSERT INTO artifacts (artifact_name, category, price, description)\n",
                 "VALUES (%s, '%s', %s, '%s')");
-        String[] queryValues = getQueryValues(messages.length, messages);
+        String[] queryValues = getQueryValues(messages);
         return String.format(query, queryValues[0], queryValues[1], queryValues[2], queryValues[3]);
     }
 
 
-    private String[] getQueryValues(int valuesSize, String[] messages) {
-        String[] values = new String[valuesSize];
-        for (int i = 0; i < valuesSize; i++) {
+    public String getCodecoolersWalletsQuery() {
+        return "SELECT first_name || last_name AS full_name, coolcoins FROM codecoolers";
+    }
+
+
+    public String markQuestDoneQuery() {
+        int[] questAndCodecoolerID = getIntQueryValues(new String[]{"Enter quest ID: ", "Enter codecoolers ID: "});
+        return String.format("INSERT INTO quests_codecoolers (id_quest, id_codecooler)%n%s",
+                String.format("VALUES (%s, %s)", questAndCodecoolerID[0], questAndCodecoolerID[1]));
+    }
+
+
+    private String[] getQueryValues(String[] messages) {
+        String[] values = new String[messages.length];
+        for (int i = 0; i < messages.length; i++) {
             values[i] = userInputs.getString(messages[i]);
+        }
+        return values;
+    }
+
+
+    private int[] getIntQueryValues(String[] messages) {
+        int[] values = new int[messages.length];
+        for (int i = 0; i < messages.length; i++) {
+            values[i] = userInputs.getInt(messages[i]);
         }
         return values;
     }
