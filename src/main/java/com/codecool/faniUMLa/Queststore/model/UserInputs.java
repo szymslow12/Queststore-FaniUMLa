272b91124ef32b4d66a5d86f6181925cdaf9dd8f
szymslow12 <szym.slow12@gmail.com>
2018-06-07 11:43:47 +0200

    add getInt(String message)

--- src/main/java/com/codecool/faniUMLa/Queststore/model/UserInputs.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/model/UserInputs.java
@@ -2,6 +2,7 @@ package com.codecool.faniUMLa.Queststore.model;
 
 import com.codecool.faniUMLa.Queststore.View;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class UserInputs {
@@ -15,5 +16,21 @@ public class UserInputs {
     public String getString(String message) {
         new View().printLine(message);
         return scanner.nextLine();
+    }
+
+
+    public int getInt(String message) {
+        boolean isCorrectInput = false;
+        int number = 0;
+        while (!isCorrectInput) {
+            try {
+                new View().printLine(message);
+                number = scanner.nextInt();
+                isCorrectInput = true;
+            } catch (InputMismatchException err) {
+                new View().printLine("Wrong input, number needed");
+            }
+        }
+        return number;
     }
 }
