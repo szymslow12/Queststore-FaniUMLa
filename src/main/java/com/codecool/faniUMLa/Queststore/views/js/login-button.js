d634f6be5d7dfe24bdb49de917e7c717e5495b13
szymslow12 <szym.slow12@gmail.com>
2018-06-19 15:52:03 +0200

    add checkIfPasswordIsEmpty()/activateSubmitButton()

--- src/main/java/com/codecool/faniUMLa/Queststore/views/js/login-button.js
+++ src/main/java/com/codecool/faniUMLa/Queststore/views/js/login-button.js
@@ -1,11 +1,25 @@
-
 function checkIfLoginIsEmpty() {
     var htmlLoginValue;
     htmlLoginValue = document.getElementById("user-login").value;
     if (htmlLoginValue.length <= 0 || htmlLoginValue.includes(" ")) {
-        setBadInputMessage("login-label", "Login cannot contain spaces!")
+        setBadInputMessage("login-label", "Login cannot contain spaces!");
+        return true;
     } else {
         setBadInputMessage("login-label", "");
+        return false;
+    }
+}
+
+
+function checkIfPasswordIsEmpty() {
+    var htmlLoginValue;
+    htmlLoginValue = document.getElementById("user-password").value;
+    if (htmlLoginValue.length <= 0) {
+        setBadInputMessage("password-label", "Password cannot be empty");
+        return true;
+    } else {
+        setBadInputMessage("password-label", "");
+        return false;
     }
 }
 
@@ -13,4 +27,13 @@ function checkIfLoginIsEmpty() {
 function setBadInputMessage(labelName, textToDisplay) {
     var loginLabel = document.getElementById(labelName);
     loginLabel.getElementsByTagName("h5")[0].innerText = textToDisplay;
+}
+
+
+function activateSubmitButton() {
+    if (!checkIfLoginIsEmpty() && !checkIfPasswordIsEmpty()) {
+        document.getElementById("login-button").disabled = false;
+    } else {
+        document.getElementById("login-button").disabled = true;
+    }
 }
