fcb877156ac8fa465e614b39b0b965bc4aa2e607
Mikolaj <mikolaj.rych@gmail.com>
2018-06-20 18:46:05 +0200

    implement functions to create and dispay forms

--- js/main.js
+++ js/main.js
@@ -11,13 +11,13 @@ function createMenu(array) {
 
     var nav = document.createElement("NAV");
     nav.setAttribute("class", "menu");
-    
+
     var ul = document.createElement("ul");
     nav.appendChild(ul);
 
     for (var i = 0; i < array.length; i++) {
         var li = document.createElement("li");
-        
+
         var a = document.createElement("a");
         a.setAttribute("href", array[i] + ".html");
         a.textContent = array[i];
@@ -39,7 +39,80 @@ function includeFooter() {
 function createButton(name) {
     var button = document.createElement("BUTTON");
     var text = document.createTextNode(name);
-    button.setAttribute("class", "button");
     button.appendChild(text);
+    button.setAttribute("class", "button");
     document.body.appendChild(button);
+    return button;
+}
+
+function createFormButton(name, inputsArray, optionsArray) {
+    
+    var button = createButton(name);
+
+    button.addEventListener("click", function () { handleForm(inputsArray, optionsArray) })
+    document.body.appendChild(button);
+}
+
+function handleForm(inputsArray, optionsArray) {
+    createForm(inputsArray, optionsArray);
+    displayForm();
+}
+
+function displayForm() {
+    var div = document.createElement("div");
+    div.setAttribute("class", "form-container");
+    var form = document.getElementById("form");
+    div.appendChild(form);
+    document.body.appendChild(div);
+}
+
+function createForm(inputsArray, optionsArray) {
+    var form = document.createElement("form");
+    form.setAttribute("id", "form");
+
+    var container = document.createElement("div");
+    container.setAttribute("class", "container");
+    createInputElements(container, inputsArray);
+    createSelectElements(container, optionsArray);
+
+    form.appendChild(container);
+    
+    var button = createButton("Save");
+    button.setAttribute("class", "button save-button");
+    form.appendChild(button);
+    document.body.appendChild(form);
+}
+
+function createInputElements(container, inputsArray) {
+    for (var i = 0; i < inputsArray.length; i++) {
+        var div = document.createElement("div");
+
+        var label = document.createElement("label");
+        div.appendChild(label);
+        label.textContent = inputsArray[i];
+
+        var input = document.createElement("input");
+        div.appendChild(input);
+
+        container.appendChild(div);
+    }
+}
+
+function createSelectElements(container, optionsArray) {
+    for (var i = 0; i < optionsArray.length; i++) {
+
+        var div = document.createElement("div");
+        var label = document.createElement("label");
+        div.appendChild(label);
+        label.textContent = optionsArray[i];
+
+        var select = document.createElement("select");
+
+        var option = document.createElement("option");
+        option.value = optionsArray[i];
+        option.text = optionsArray[i];
+        select.appendChild(option);
+        div.appendChild(select);
+        container.appendChild(div);
+    }
 }
