cc92dec85358574168f75807dc718435ff05533b
Mikolaj <mikolaj.rych@gmail.com>
2018-06-22 00:34:17 +0200

    implement login as admin, mentor, student

--- js/login-button.js
+++ js/login-button.js
@@ -1,7 +1,7 @@
 function checkIfLoginIsEmpty() {
     var htmlLoginValue;
     htmlLoginValue = document.getElementById("user-login").value;
-    if (htmlLoginValue.length <= 0 || htmlLoginValue.includes(" ")) {
+    if (htmlLoginValue.length == 0 || htmlLoginValue.includes(" ")) {
         document.getElementById("password-label-container").style.top = "-50px";
         document.getElementById("user-login").style.border = "1px solid #33CCCC";
         setBadInputMessage("login-label", "Login cannot contain spaces!");
@@ -18,7 +18,7 @@ function checkIfLoginIsEmpty() {
 function checkIfPasswordIsEmpty() {
     var htmlLoginValue;
     htmlLoginValue = document.getElementById("user-password").value;
-    if (htmlLoginValue.length <= 0) {
+    if (htmlLoginValue.length == 0) {
         setBadInputMessage("password-label", "Password cannot be empty!");
         document.getElementById("user-password").style.border = "1px solid #33CCCC";
         return true;
@@ -37,8 +37,11 @@ function setBadInputMessage(labelName, textToDisplay) {
 
 
 function activateSubmitButton() {
-    if (!checkIfLoginIsEmpty() && !checkIfPasswordIsEmpty()) {
-        document.getElementById("login-button").disabled = false;
+    var a = checkIfLoginIsEmpty();
+    var b = checkIfPasswordIsEmpty();
+    if (!a && !b) {
+        window.alert(a);
+        document.getElementById("login-button").setAttribute("disabled", false);
     } else {
         document.getElementById("login-button").disabled = true;
     }
@@ -48,14 +51,22 @@ function activateSubmitButton() {
 
 function loginToQuestStore() {
     var login, password, lol;
+    var form = document.getElementById("login");
     login = document.forms["login"]["login"].value;
     password = document.forms["login"]["password"].value;
-    if (login != "test" && password != "test") {
-        document.getElementById("input-container").style.top = "0px";
-        document.getElementById("login-head").getElementsByTagName("h5")[0].innerText = "Bad login or password!";
-        return false;
-    } else {
+    // if (login != "test" && password != "test") {
+    //     document.getElementById("input-container").style.top = "0px";
+    //     document.getElementById("login-head").getElementsByTagName("h5")[0].innerText = "Bad login or password!";
+    //     return false;
+    // } else
+    if (login == "admin" && password == "admin") {
+        form.setAttribute("action", "../html/admin/mentors.html");
         return true;
+    }
+    else if (login == "mentor" && password == "mentor") {
+        form.setAttribute("action", "../html/mentor/Students.html");
+    } else if (login == "student" && password == "student") {
+        form.setAttribute("action", "../html/student/Store.html");
     }
 }
 
@@ -64,7 +75,7 @@ function adjustPositionOfSubmitButton() {
     var login, password;
     login = document.getElementById("login-label").getElementsByTagName("h5")[0].innerText;
     password = document.getElementById("password-label").getElementsByTagName("h5")[0].innerText;
-    if (login != "" && password != ""){
+    if (login != "" && password != "") {
         document.getElementById("login-button-container").style.top = "-121px";
     } else if (login != "" || password != "") {
         document.getElementById("login-button-container").style.top = "-60px";
