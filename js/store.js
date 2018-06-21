f495db2152339d2c541976ae25504cfd2faa8e5a
szymslow12 <szym.slow12@gmail.com>
2018-06-21 09:55:51 +0200

    add createStoreButtons()/createButton(name)

---
+++ js/store.js
@@ -0,0 +1,20 @@
+
+
+function createStoreButtons(array) {
+    var buttonContainer = document.createElement("DIV");
+    buttonContainer.setAttribute("class", "button-container");
+    for (i = 0; i < array.length; i++) {
+        buttonContainer.appendChild(createButton(array[i]));
+    }
+
+    document.body.appendChild(buttonContainer);
+}
+
+function createButton(name) {
+    var button = document.createElement("BUTTON");
+    var text = document.createTextNode(name);
+    button.appendChild(text);
+    button.setAttribute("class", "button");
+    document.body.appendChild(button);
+    return button;
+}
