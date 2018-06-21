3335b09e1a20043610b8d6eac1aa1b8e872b24fa
szymslow12 <szym.slow12@gmail.com>
2018-06-21 11:02:42 +0200

    add setAttribute with button id

--- js/store.js
+++ js/store.js
@@ -3,18 +3,20 @@
 function createStoreButtons(array) {
     var buttonContainer = document.createElement("DIV");
     buttonContainer.setAttribute("class", "button-container");
+    var buttonIDs = ['basic-items', 'magic-items'];
     for (i = 0; i < array.length; i++) {
-        buttonContainer.appendChild(createButton(array[i]));
+        buttonContainer.appendChild(createButton(array[i], buttonIDs[i]));
     }
 
     document.body.appendChild(buttonContainer);
 }
 
-function createButton(name) {
+function createButton(name, id) {
     var button = document.createElement("BUTTON");
     var text = document.createTextNode(name);
     button.appendChild(text);
     button.setAttribute("class", "button");
+    button.setAttribute("id", id);
     button.setAttribute("onclick", "createStoreTable(['Name', 'Description', 'Price', 'Buy'])");
     document.body.appendChild(button);
     return button;
