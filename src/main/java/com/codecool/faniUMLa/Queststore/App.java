dd55b2b2df21b4028a42e51572f04c6eff3cd726
szymslow12 <szym.slow12@gmail.com>
2018-07-02 19:24:33 +0200

    add ServerConnection in main()

--- src/main/java/com/codecool/faniUMLa/Queststore/App.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/App.java
@@ -1,11 +1,13 @@
 package com.codecool.faniUMLa.Queststore;
 
 import com.codecool.faniUMLa.Queststore.controllers.AppController;
+import com.codecool.faniUMLa.Queststore.controllers.ServerConnection;
 
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Queststore");
+        ServerConnection serverConnection = new ServerConnection(2020);
         AppController controller = new AppController();
         controller.run();
     }
