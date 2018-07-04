f0f8cee0721657eee0ad7e45ce3a5e1695b7ded1
Mikolaj <mikolaj.rych@gmail.com>
2018-07-04 12:00:38 +0200

    add daoMentorController as server context

--- src/main/java/com/codecool/faniUMLa/Queststore/Server.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/Server.java
@@ -1,5 +1,6 @@
 package com.codecool.faniUMLa.Queststore;
 import com.codecool.faniUMLa.Queststore.controllers.DAOAdminController;
+import com.codecool.faniUMLa.Queststore.controllers.DAOMentorController;
 import com.codecool.faniUMLa.Queststore.server.*;
 
 import com.codecool.faniUMLa.Queststore.controllers.AppController;
@@ -23,6 +24,7 @@ public class Server {
         server.createContext("/js/main.js", new Static());
         server.createContext("/js/login-button.js", new Static());
         server.createContext("/daoAdminController", new DAOAdminController());
+        server.createContext("/daoMentorController", new DAOMentorController());
         server.createContext("/js/store.js", new Static());
         server.createContext("/css/main.css", new Static());
         server.setExecutor(null);
