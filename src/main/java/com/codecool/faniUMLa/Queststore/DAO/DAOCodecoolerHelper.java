853a35d04bb3eafb0eb67a31b859fd2f25b6d578
Mikolaj <mikolaj.rych@gmail.com>
2018-07-05 22:25:06 +0200

    fix typos

--- src/main/java/com/codecool/faniUMLa/Queststore/DAO/DAOCodecoolerHelper.java
+++ src/main/java/com/codecool/faniUMLa/Queststore/DAO/DAOCodecoolerHelper.java
@@ -90,7 +90,7 @@ public class DAOCodecoolerHelper {
             try {
                 rs = studentArtQuery(idUser).executeQuery();
                 if (rs.next()) {
-                    if (rs.getInt("id_artifact") != artifact.getCategory().getCategoryID()) {
+                    if (rs.getInt("id_artifact") != artifact.getCategory()) {
                         int price = artifact.getPrice();
                         int money = getCoolcoins(idUser);
                         if (isAvailable(price, money)) {
@@ -131,7 +131,7 @@ public class DAOCodecoolerHelper {
     }
 
     private boolean isSingleBuyer(Artifact artifact) {
-        return artifact.getCategory().getCategoryID() == 1;
+        return artifact.getCategory() == 1;
     }
 
     public List<Artifact> showArtifacts() {
@@ -154,7 +154,7 @@ public class DAOCodecoolerHelper {
                 int category_id = rs.getInt("category_id");
                 int price = rs.getInt("price");
                 String description = rs.getString("description");
-                artifacts.add(new Artifact(id, name, new ArtifactCategory(category_id), price, description));
+                artifacts.add(new Artifact(id, name, category_id, price, description));
             }
         } catch (SQLException e) {
             e.printStackTrace();
@@ -299,7 +299,7 @@ public class DAOCodecoolerHelper {
                 int quantity = resultSet.getInt("quantity");
                 String name = resultSet.getString("artifact_name");
                 String description = resultSet.getString("description");
-                boughtArtifacts.add(new Artifact(artifactID, name, new ArtifactCategory(categoryID),price, description));
+                boughtArtifacts.add(new Artifact(artifactID, name, categoryID,price, description));
                 artifactsQuantity.put(artifactID, quantity);
             }
 
