1a1fd6352544f45dd4bc1ffd55f5f3272d727c4b
wluka85 <wluka85@gmail.com>
2018-07-18 14:39:58 +0200

    Implement method of testBuyArtifact

--- src/test/java/com/codecool/faniUMLa/Queststore/DAO/DAOCodecoolerTest.java
+++ src/test/java/com/codecool/faniUMLa/Queststore/DAO/DAOCodecoolerTest.java
@@ -71,5 +71,35 @@ class DAOCodecoolerTest {
         assertEquals(expectedLevel, actualLevel);
     }
 
+    @Test
+    void testBuyArtifactIfEnoughtMoney() throws SQLException {
+        int idArtifact = 1;
+        int artifactPrice = 30;
+        int userCoolcoins = 40;
+
+        generateMocksOfBuyArtifact(artifactPrice, userCoolcoins);
+
+        boolean isBoughtArtifact = daoCodecooler.buyArtifact(userId, idArtifact);
+        assertTrue(isBoughtArtifact);
+    }
+
+    @Test
+    void testBuyArtifactIfNotEnoughtMoney() throws SQLException {
+        int idArtifact = 1;
+        int artifactPrice = 20;
+        int userCoolcoins = 10;
+
+        generateMocksOfBuyArtifact(artifactPrice, userCoolcoins);
+
+        boolean isBoughtArtifact = daoCodecooler.buyArtifact(userId, idArtifact);
+        assertFalse(isBoughtArtifact);
+    }
+
+    private void generateMocksOfBuyArtifact(int artifactPrice, int userCoolcoins) throws SQLException {
+        when(connection.prepareStatement(any(String.class))).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+        when(resultSet.getInt(any(Integer.class))).thenReturn(artifactPrice);
+        when(resultSet.getInt("coolcoins")).thenReturn(userCoolcoins);
+    }
 
 }
