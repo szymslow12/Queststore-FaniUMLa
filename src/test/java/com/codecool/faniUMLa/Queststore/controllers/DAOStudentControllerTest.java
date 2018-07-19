c757d69c57854a8cc4c93460c93588ed0ded4ba1
Łukasz <35366441+wluka85@users.noreply.github.com>
2018-07-19 21:40:03 +0200

    Tests (#9)
    * Rename method
    
    * Implement test of inventory
    
    * Add needed files
    
    * Add nedded files to test
    
    * Add file workspace.xml

--- src/test/java/com/codecool/faniUMLa/Queststore/controllers/DAOStudentControllerTest.java
+++ src/test/java/com/codecool/faniUMLa/Queststore/controllers/DAOStudentControllerTest.java
@@ -3,6 +3,7 @@ package com.codecool.faniUMLa.Queststore.controllers;
 
 import com.codecool.faniUMLa.Queststore.DAO.DAOCodecooler;
 import com.codecool.faniUMLa.Queststore.model.store.Artifact;
+import com.codecool.faniUMLa.Queststore.model.store.Inventory;
 import com.sun.net.httpserver.HttpExchange;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -24,7 +25,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 import static org.mockito.Mockito.when;
@@ -47,44 +50,121 @@ class DAOStudentControllerTest {
     private DAOCodecooler mockDaoCodecooler;
 
     private DAOStudentController daoStudentController;
+    private OutputStream outputStream;
 
 
     @BeforeEach
     void setUp() throws SQLException {
         MockitoAnnotations.initMocks(this);
+        daoStudentController = new DAOStudentController(mockConnection, mockDaoCodecooler);
+        when(mockHttpExchange.getRequestMethod()).thenReturn("GET");
+        outputStream = new ByteArrayOutputStream();
     }
 
     @Test
-    void testGetStudentCoolcoins() {
+    void testGenerateResponseOfStudentCoolcoins() {
         int coolcoins = 50;
+        String response = getStudentCoolcoinsResponse(coolcoins);
 
-        JSONArray json = new JSONArray();
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("Coolcoins", coolcoins);
-        json.put(jsonObject);
-        String response = json.toString();
-
-        daoStudentController = new DAOStudentController(mockConnection, mockDaoCodecooler);
-        when(mockHttpExchange.getRequestMethod()).thenReturn("GET");
         URI uri = URI.create("/daoStudentController?Method=Coolcoins");
         when(mockHttpExchange.getRequestURI()).thenReturn(uri);
         when(mockDaoCodecooler.getCoolcoins(any(Integer.class))).thenReturn(coolcoins);
-
-        OutputStream outputStream = new ByteArrayOutputStream();
         when(mockHttpExchange.getResponseBody()).thenReturn(outputStream);
-
         daoStudentController.handle(mockHttpExchange);
 
         assertEquals(response, outputStream.toString());
     }
 
     @Test
-    void testGetArtifactsByCategory() {
-        int categoryId = 1;
-        List<Artifact> artifactList = new ArrayList<>();
-        artifactList.add(new Artifact(1, "Artifact 1", 1, 20, "Description"));
-        artifactList.add(new Artifact(2, "Artifact 2", 1, 30, "Description"));
+    void testGenerateResponseOfArtifactsByCategory() {
+        List<Artifact> artifactList = getArtifacts();
+        String response = getResponseOfArtifacts(artifactList);
 
+        URI uri = URI.create("/daoStudentController?Method=Store2");
+        when(mockHttpExchange.getRequestURI()).thenReturn(uri);
+        when(mockDaoCodecooler.showArtifacts(any(Integer.class))).thenReturn(artifactList);
+        when(mockHttpExchange.getResponseBody()).thenReturn(outputStream);
+        daoStudentController.handle(mockHttpExchange);
+
+        assertEquals(response, outputStream.toString());
+    }
+
+    @Test
+    void testGetArtifactsByCategoryIfArtifactListIsEmpty() {
+        List<Artifact> artifactList = new ArrayList<>();
+
+        URI uri = URI.create("/daoStudentController?Method=Store2");
+        when(mockHttpExchange.getRequestURI()).thenReturn(uri);
+        when(mockDaoCodecooler.showArtifacts(any(Integer.class))).thenReturn(artifactList);
+        when(mockHttpExchange.getResponseBody()).thenReturn(outputStream);
+        daoStudentController.handle(mockHttpExchange);
+
+        assertNotNull(outputStream.toString());
+    }
+
+    @Test
+    void testGenerateResponseOfInventory() {
+        List<Artifact> actualArtifactList = getArtifacts();
+        Map<Integer, Integer> artifactQuantityMap = getArtifactQuantityMap();
+        Inventory inventory = new Inventory(actualArtifactList, artifactQuantityMap);
+
+        URI uri = URI.create("/daoStudentController?Method=Inventory");
+        when(mockHttpExchange.getRequestURI()).thenReturn(uri);
+        when(mockDaoCodecooler.getBoughtArtifacts(any(Integer.class))).thenReturn(inventory);
+        when(mockHttpExchange.getResponseBody()).thenReturn(outputStream);
+        daoStudentController.handle(mockHttpExchange);
+
+        String[] expectedStreamArray = getExpectedStreamArray(outputStream);
+        Map<String, String> artifact1Map = getMapOfArtifactAtributs(expectedStreamArray[0]);
+        Map<String, String> artifact2Map = getMapOfArtifactAtributs(expectedStreamArray[1]);
+
+        assertArtifact(artifact1Map, actualArtifactList.get(0));
+        assertArtifact(artifact2Map, actualArtifactList.get(1));
+
+    }
+
+    private void assertArtifact(Map<String, String> artifactFieldMap, Artifact actualArtifact) {
+        assertEquals(actualArtifact.getName(), artifactFieldMap.get("Name"));
+        assertEquals(actualArtifact.getPrice(), Integer.parseInt(artifactFieldMap.get("Price")));
+        assertEquals(actualArtifact.getDescription(), artifactFieldMap.get("Description"));
+    }
+
+    private String[] getExpectedStreamArray(OutputStream outputStream) {
+        String expectedStream = outputStream.toString();
+        expectedStream = expectedStream.replaceAll("\\[\\{", "");
+        expectedStream = expectedStream.replaceAll("}]", "");
+        expectedStream = expectedStream.replaceAll("\"", "");
+        return expectedStream.split("},\\{");
+    }
+
+    private Map<Integer, Integer> getArtifactQuantityMap() {
+        Map<Integer, Integer> artifactQuantityMap = new HashMap<>();
+        artifactQuantityMap.put(1, 2);
+        artifactQuantityMap.put(2, 3);
+        return artifactQuantityMap;
+    }
+
+    private Map getMapOfArtifactAtributs(String text) {
+        Map<String, String> atributMap = new HashMap<>();
+        String[] atributArray = text.split(",");
+        for(String atribut : atributArray) {
+            String[] fieldArray = atribut.split(":");
+            atributMap.put(fieldArray[0], fieldArray[1]);
+        }
+
+        return atributMap;
+
+    }
+
+    private String getStudentCoolcoinsResponse(int coolcoins) {
+        JSONArray json = new JSONArray();
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("Coolcoins", coolcoins);
+        json.put(jsonObject);
+        return json.toString();
+    }
+
+    private String getResponseOfArtifacts(List<Artifact> artifactList) {
         JSONArray json = new JSONArray();
         for (Artifact artifact: artifactList) {
             JSONObject jsonObject = new JSONObject();
@@ -95,39 +175,13 @@ class DAOStudentControllerTest {
             json.put(jsonObject);
         }
 
-        String response = json.toString();
-
-        daoStudentController = new DAOStudentController(mockConnection, mockDaoCodecooler);
-        when(mockHttpExchange.getRequestMethod()).thenReturn("GET");
-        URI uri = URI.create("/daoStudentController?Method=Store2");
-        when(mockHttpExchange.getRequestURI()).thenReturn(uri);
-        when(mockDaoCodecooler.showArtifacts(any(Integer.class))).thenReturn(artifactList);
-
-        OutputStream outputStream = new ByteArrayOutputStream();
-        when(mockHttpExchange.getResponseBody()).thenReturn(outputStream);
-
-        daoStudentController.handle(mockHttpExchange);
-
-        assertEquals(response, outputStream.toString());
+        return json.toString();
     }
 
-    @Test
-    void testGetArtifactsByCategoryIfListNull() {
-        int categoryId = 1;
-
+    private List<Artifact> getArtifacts() {
         List<Artifact> artifactList = new ArrayList<>();
-
-        daoStudentController = new DAOStudentController(mockConnection, mockDaoCodecooler);
-        when(mockHttpExchange.getRequestMethod()).thenReturn("GET");
-        URI uri = URI.create("/daoStudentController?Method=Store2");
-        when(mockHttpExchange.getRequestURI()).thenReturn(uri);
-        when(mockDaoCodecooler.showArtifacts(any(Integer.class))).thenReturn(artifactList);
-
-        OutputStream outputStream = new ByteArrayOutputStream();
-        when(mockHttpExchange.getResponseBody()).thenReturn(outputStream);
-
-        daoStudentController.handle(mockHttpExchange);
-
-        assertNotNull(outputStream.toString());
+        artifactList.add(new Artifact(1, "Artifact 1", 1, 20, "Description"));
+        artifactList.add(new Artifact(2, "Artifact 2", 1, 30, "Description"));
+        return artifactList;
     }
 }
